9c91a4f9bea4aa6ab9a1df52a3c5d3c70e40448b
Xiao YaoBing <1109050030@qq.com>
2024-04-02 14:19:47 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # wlscenegraph
-open source high performance 3D graphics toolkit for Wayland compositor
+provides a open source high performance 3D graphics toolkit for Wayland compositor. It is based on the wlroots Wayland compositor library, which allows development of automated rendering and image post-processing based on wlroots, while supporting modern window animations.guidelines are based on performance and supplemented by design.
+
+## Release Signatures
+
+## Installation
+Run these commands to make install :
+```shell
+    meson build/ --prefix=/usr --buildtype=debug
+    sudo ninja -C build/ install
+```
+
+## doxygen
+
+### From Packages
+
+### Compiling from Source
+Install dependencies:
+
+build dependencies(for arch linux):
+* meson \*
+* [wlroots]
+  
+## Configuration
+
+## Running
+ref example
+
+## How to Contribute
+* Contributing just involves sending a merge request.
+* Note: rules are made to be broken. Adjust or ignore any/all of these as you see
+fit, but be prepared to justify it to your peers.
+
+### Code Style
+* wsm is written in C with a style similar to the kernel style, but with a
+few notable differences.
+* Try to keep your code conforming to C11 and POSIX as much as possible, and do
+not use GNU extensions.
+
+### Code Architecture Guidelines
+* The code should be simple and easy to understand.
+* Add comments to key nodes whether you change or add new code
+* Security > Compatibility > Extensibility >= Performance
+
+### Contribution Guideline
+* Contribution steps.
+    1. First login to your Github account and fork the project
+    2. Pull the forked project locally using `git clone`.
+    3. Push the new commit to your project using `git push`.
+    4. commit your code to the upstream project on Github using the Pull Requese feature.
+* commit message specification: use English. Be sure to describe exactly what the commit "does" and "why it was made"
+* A commit only does one thing, and the smaller the code changes, the easier it is to accept the commit. For larger code changes, try to split the commit into multiple commits (satisfying the git commit principle as a prerequisite)
+* Please do your own testing and code review before committing the code, and submit the PR after confirming that the code is working correctly
+
+## Roadmap
+* Support scissor window
+* Support window backblur
+* Supprot window animator
+* Support particle simulations
---
+++ tinywl/tinywl.c
@@ -0,0 +1,385 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <wayland-server-core.h>
+#include <wlr/backend.h>
+#include <wlr/render/allocator.h>
+#include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_cursor.h>
+#include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_input_device.h>
+#include <wlr/types/wlr_keyboard.h>
+#include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_pointer.h>
+#include <wlr/types/wlr_scene.h>
+#include <wlr/types/wlr_seat.h>
+#include <wlr/types/wlr_subcompositor.h>
+#include <wlr/types/wlr_xcursor_manager.h>
+#include <wlr/util/log.h>
+#include <xkbcommon/xkbcommon.h>
+
+enum tinywl_cursor_mode {
+	TINYWL_CURSOR_PASSTHROUGH,
+	TINYWL_CURSOR_MOVE,
+	TINYWL_CURSOR_RESIZE,
+};
+
+struct tinywl_server {
+	struct wl_display *wl_display;
+	struct wlr_backend *backend;
+	struct wlr_renderer *renderer;
+	struct wlr_allocator *allocator;
+	struct wlr_scene *scene;
+	struct wlr_scene_output_layout *scene_layout;
+
+	struct wl_listener new_xdg_surface;
+	struct wl_list toplevels;
+
+	struct wlr_cursor *cursor;
+	struct wlr_xcursor_manager *cursor_mgr;
+	struct wl_listener cursor_motion;
+	struct wl_listener cursor_motion_absolute;
+	struct wl_listener cursor_button;
+	struct wl_listener cursor_axis;
+	struct wl_listener cursor_frame;
+
+	struct wlr_seat *seat;
+	struct wl_listener new_input;
+	struct wl_listener request_cursor;
+	struct wl_listener request_set_selection;
+	struct wl_list keyboards;
+	enum tinywl_cursor_mode cursor_mode;
+	struct tinywl_toplevel *grabbed_toplevel;
+	double grab_x, grab_y;
+	struct wlr_box grab_geobox;
+	uint32_t resize_edges;
+
+	struct wlr_output_layout *output_layout;
+	struct wl_list outputs;
+	struct wl_listener new_output;
+};
+
+struct tinywl_output {
+	struct wl_list link;
+	struct tinywl_server *server;
+	struct wlr_output *wlr_output;
+	struct wl_listener frame;
+	struct wl_listener request_state;
+	struct wl_listener destroy;
+};
+
+struct tinywl_keyboard {
+	struct wl_list link;
+	struct tinywl_server *server;
+	struct wlr_keyboard *wlr_keyboard;
+
+	struct wl_listener modifiers;
+	struct wl_listener key;
+	struct wl_listener destroy;
+};
+
+static void server_new_pointer(struct tinywl_server *server,
+		struct wlr_input_device *device) {
+	wlr_cursor_attach_input_device(server->cursor, device);
+}
+
+static void server_new_input(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server =
+		wl_container_of(listener, server, new_input);
+	struct wlr_input_device *device = data;
+	switch (device->type) {
+	case WLR_INPUT_DEVICE_POINTER:
+		server_new_pointer(server, device);
+		break;
+	default:
+		break;
+	}
+	uint32_t caps = WL_SEAT_CAPABILITY_POINTER;
+	if (!wl_list_empty(&server->keyboards)) {
+		caps |= WL_SEAT_CAPABILITY_KEYBOARD;
+	}
+	wlr_seat_set_capabilities(server->seat, caps);
+}
+
+static void seat_request_cursor(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server = wl_container_of(
+            listener, server, request_cursor);
+	struct wlr_seat_pointer_request_set_cursor_event *event = data;
+	struct wlr_seat_client *focused_client =
+        server->seat->pointer_state.focused_client;
+    if (focused_client == event->seat_client) {
+		wlr_cursor_set_surface(server->cursor, event->surface,
+				event->hotspot_x, event->hotspot_y);
+	}
+}
+
+static void seat_request_set_selection(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server = wl_container_of(
+			listener, server, request_set_selection);
+	struct wlr_seat_request_set_selection_event *event = data;
+	wlr_seat_set_selection(server->seat, event->source, event->serial);
+}
+
+static struct tinywl_toplevel *desktop_toplevel_at(
+		struct tinywl_server *server, double lx, double ly,
+		struct wlr_surface **surface, double *sx, double *sy) {
+	struct wlr_scene_node *node = wlr_scene_node_at(
+		&server->scene->tree.node, lx, ly, sx, sy);
+	if (node == NULL || node->type != WLR_SCENE_NODE_BUFFER) {
+		return NULL;
+	}
+	struct wlr_scene_buffer *scene_buffer = wlr_scene_buffer_from_node(node);
+	struct wlr_scene_surface *scene_surface =
+		wlr_scene_surface_try_from_buffer(scene_buffer);
+	if (!scene_surface) {
+		return NULL;
+	}
+
+    *surface = scene_surface->surface;
+	struct wlr_scene_tree *tree = node->parent;
+	while (tree != NULL && tree->node.data == NULL) {
+		tree = tree->node.parent;
+	}
+	return tree->node.data;
+}
+
+static void process_cursor_motion(struct tinywl_server *server, uint32_t time) {
+	double sx, sy;
+	struct wlr_seat *seat = server->seat;
+	struct wlr_surface *surface = NULL;
+	struct tinywl_toplevel *toplevel = desktop_toplevel_at(server,
+			server->cursor->x, server->cursor->y, &surface, &sx, &sy);
+    if (!toplevel) {
+		wlr_cursor_set_xcursor(server->cursor, server->cursor_mgr, "default");
+	}
+    if (surface) {
+		wlr_seat_pointer_notify_enter(seat, surface, sx, sy);
+		wlr_seat_pointer_notify_motion(seat, time, sx, sy);
+    } else {
+		wlr_seat_pointer_clear_focus(seat);
+	}
+}
+
+static void server_cursor_motion(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server =
+		wl_container_of(listener, server, cursor_motion);
+	struct wlr_pointer_motion_event *event = data;
+	wlr_cursor_move(server->cursor, &event->pointer->base,
+			event->delta_x, event->delta_y);
+	process_cursor_motion(server, event->time_msec);
+}
+
+static void server_cursor_motion_absolute(
+		struct wl_listener *listener, void *data) {
+	struct tinywl_server *server =
+		wl_container_of(listener, server, cursor_motion_absolute);
+	struct wlr_pointer_motion_absolute_event *event = data;
+	wlr_cursor_warp_absolute(server->cursor, &event->pointer->base, event->x,
+		event->y);
+	process_cursor_motion(server, event->time_msec);
+}
+
+static void server_cursor_axis(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server =
+		wl_container_of(listener, server, cursor_axis);
+    struct wlr_pointer_axis_event *event = data;
+	wlr_seat_pointer_notify_axis(server->seat,
+			event->time_msec, event->orientation, event->delta,
+			event->delta_discrete, event->source);
+}
+
+static void server_cursor_frame(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server =
+        wl_container_of(listener, server, cursor_frame);
+	wlr_seat_pointer_notify_frame(server->seat);
+}
+
+static void output_frame(struct wl_listener *listener, void *data) {
+	struct tinywl_output *output = wl_container_of(listener, output, frame);
+	struct wlr_scene *scene = output->server->scene;
+
+	struct wlr_scene_output *scene_output = wlr_scene_get_scene_output(
+		scene, output->wlr_output);
+
+	wlr_scene_output_commit(scene_output, NULL);
+
+	struct timespec now;
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	wlr_scene_output_send_frame_done(scene_output, &now);
+}
+
+static void output_request_state(struct wl_listener *listener, void *data) {
+	struct tinywl_output *output = wl_container_of(listener, output, request_state);
+	const struct wlr_output_event_request_state *event = data;
+	wlr_output_commit_state(output->wlr_output, event->state);
+}
+
+static void output_destroy(struct wl_listener *listener, void *data) {
+	struct tinywl_output *output = wl_container_of(listener, output, destroy);
+
+	wl_list_remove(&output->frame.link);
+	wl_list_remove(&output->request_state.link);
+	wl_list_remove(&output->destroy.link);
+	wl_list_remove(&output->link);
+	free(output);
+}
+
+static void server_new_output(struct wl_listener *listener, void *data) {
+	struct tinywl_server *server =
+		wl_container_of(listener, server, new_output);
+	struct wlr_output *wlr_output = data;
+
+	wlr_output_init_render(wlr_output, server->allocator, server->renderer);
+
+	struct wlr_output_state state;
+	wlr_output_state_init(&state);
+	wlr_output_state_set_enabled(&state, true);
+
+	struct wlr_output_mode *mode = wlr_output_preferred_mode(wlr_output);
+	if (mode != NULL) {
+		wlr_output_state_set_mode(&state, mode);
+	}
+
+	wlr_output_commit_state(wlr_output, &state);
+	wlr_output_state_finish(&state);
+
+	struct tinywl_output *output = calloc(1, sizeof(*output));
+	output->wlr_output = wlr_output;
+	output->server = server;
+
+	output->frame.notify = output_frame;
+	wl_signal_add(&wlr_output->events.frame, &output->frame);
+
+	output->request_state.notify = output_request_state;
+	wl_signal_add(&wlr_output->events.request_state, &output->request_state);
+
+	output->destroy.notify = output_destroy;
+	wl_signal_add(&wlr_output->events.destroy, &output->destroy);
+
+	wl_list_insert(&server->outputs, &output->link);
+
+	struct wlr_output_layout_output *l_output = wlr_output_layout_add_auto(server->output_layout,
+		wlr_output);
+	struct wlr_scene_output *scene_output = wlr_scene_output_create(server->scene, wlr_output);
+	wlr_scene_output_layout_add_output(server->scene_layout, l_output, scene_output);
+}
+
+int main(int argc, char *argv[]) {
+	wlr_log_init(WLR_DEBUG, NULL);
+	char *startup_cmd = NULL;
+
+	int c;
+	while ((c = getopt(argc, argv, "s:h")) != -1) {
+		switch (c) {
+		case 's':
+			startup_cmd = optarg;
+			break;
+		default:
+			printf("Usage: %s [-s startup command]\n", argv[0]);
+			return 0;
+		}
+	}
+	if (optind < argc) {
+		printf("Usage: %s [-s startup command]\n", argv[0]);
+		return 0;
+	}
+
+    struct tinywl_server server = {0};
+    server.wl_display = wl_display_create();
+	server.backend = wlr_backend_autocreate(server.wl_display, NULL);
+	if (server.backend == NULL) {
+		wlr_log(WLR_ERROR, "failed to create wlr_backend");
+		return 1;
+    }
+	server.renderer = wlr_renderer_autocreate(server.backend);
+	if (server.renderer == NULL) {
+		wlr_log(WLR_ERROR, "failed to create wlr_renderer");
+		return 1;
+	}
+
+	wlr_renderer_init_wl_display(server.renderer, server.wl_display);
+
+	server.allocator = wlr_allocator_autocreate(server.backend,
+		server.renderer);
+	if (server.allocator == NULL) {
+		wlr_log(WLR_ERROR, "failed to create wlr_allocator");
+		return 1;
+	}
+
+	wlr_compositor_create(server.wl_display, 5, server.renderer);
+	wlr_subcompositor_create(server.wl_display);
+	wlr_data_device_manager_create(server.wl_display);
+
+	server.output_layout = wlr_output_layout_create();
+
+	wl_list_init(&server.outputs);
+	server.new_output.notify = server_new_output;
+	wl_signal_add(&server.backend->events.new_output, &server.new_output);
+
+	server.scene = wlr_scene_create();
+	server.scene_layout = wlr_scene_attach_output_layout(server.scene, server.output_layout);
+
+    wl_list_init(&server.toplevels);
+	server.cursor = wlr_cursor_create();
+	wlr_cursor_attach_output_layout(server.cursor, server.output_layout);
+
+	server.cursor_mgr = wlr_xcursor_manager_create(NULL, 24);
+	server.cursor_mode = TINYWL_CURSOR_PASSTHROUGH;
+	server.cursor_motion.notify = server_cursor_motion;
+	wl_signal_add(&server.cursor->events.motion, &server.cursor_motion);
+	server.cursor_motion_absolute.notify = server_cursor_motion_absolute;
+	wl_signal_add(&server.cursor->events.motion_absolute,
+			&server.cursor_motion_absolute);
+    // server.cursor_button.notify = server_cursor_button;
+	wl_signal_add(&server.cursor->events.button, &server.cursor_button);
+	server.cursor_axis.notify = server_cursor_axis;
+	wl_signal_add(&server.cursor->events.axis, &server.cursor_axis);
+	server.cursor_frame.notify = server_cursor_frame;
+	wl_signal_add(&server.cursor->events.frame, &server.cursor_frame);
+	wl_list_init(&server.keyboards);
+	server.new_input.notify = server_new_input;
+	wl_signal_add(&server.backend->events.new_input, &server.new_input);
+	server.seat = wlr_seat_create(server.wl_display, "seat0");
+	server.request_cursor.notify = seat_request_cursor;
+	wl_signal_add(&server.seat->events.request_set_cursor,
+			&server.request_cursor);
+	server.request_set_selection.notify = seat_request_set_selection;
+	wl_signal_add(&server.seat->events.request_set_selection,
+			&server.request_set_selection);
+
+	const char *socket = wl_display_add_socket_auto(server.wl_display);
+	if (!socket) {
+		wlr_backend_destroy(server.backend);
+		return 1;
+	}
+
+	if (!wlr_backend_start(server.backend)) {
+		wlr_backend_destroy(server.backend);
+		wl_display_destroy(server.wl_display);
+		return 1;
+	}
+
+	setenv("WAYLAND_DISPLAY", socket, true);
+	if (startup_cmd) {
+		if (fork() == 0) {
+			execl("/bin/sh", "/bin/sh", "-c", startup_cmd, (void *)NULL);
+		}
+	}
+
+	wlr_log(WLR_INFO, "Running Wayland compositor on WAYLAND_DISPLAY=%s",
+			socket);
+	wl_display_run(server.wl_display);
+
+	wl_display_destroy_clients(server.wl_display);
+	wlr_scene_node_destroy(&server.scene->tree.node);
+	wlr_xcursor_manager_destroy(server.cursor_mgr);
+	wlr_output_layout_destroy(server.output_layout);
+	wl_display_destroy(server.wl_display);
+	return 0;
+}
+
